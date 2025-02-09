f94013a9ac9708eb5324df5c70c68235950abde4
LeeJiwoo982 <easywoo980902@gmail.com>
2025-02-09 20:47:52 +0900

    update 1860 files

--- LeeJiwooIM/SWea/1860_bestFish/s1860.py
+++ LeeJiwooIM/SWea/1860_bestFish/s1860.py
@@ -5,6 +5,7 @@ sys.stdin = open('input.txt', 'r')
 # 0초부터 만들기 시작 M초의 시간에 K개의 붕어빵을 만듬
 # 완성과 함께 다음 붕어빵 만들기 가능
 T = int(input())
-for tc in range(1, T+1):
-    N, M, K = map(int, input().split())
-    pass
+for tc in range(1, T+1): # 1
+    N, M, K = map(int, input().split()) [2,2,2]
+    # 손님 정보 배열 밭기
+    customers = list(map(int, input().split())) #[3, 4]
