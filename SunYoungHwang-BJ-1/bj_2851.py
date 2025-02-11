052df02804dc5993d172eca22bd6763a0359c2fe
sunny <fruitback7@gmail.com>
2025-02-12 08:16:24 +0900

    250212

--- SunYoungHwang-BJ-1/bj_2851.py
+++ SunYoungHwang-BJ-1/bj_2851.py
@@ -1,16 +1,17 @@
 # 슈퍼마리오
 
 mush = [int(input()) for _ in range(10)]
-sum_mush = 0
-for i in range(10):
-    sum_mush += mush[i]
-    if sum_mush == 100:
-        print(sum_mush)
-        break
-    elif sum_mush-mush[i]<100 and sum_mush > 100:
-        if abs(sum_mush-100) <= abs(100-(sum_mush-mush[i])):
-            print(sum_mush)
-            break
-        else:
-            print(sum_mush-mush[i])
-            break
+
+def mush_sum(arr):
+    sum_mush = 0
+    closest = 0
+    for mush in arr:
+        sum_mush += mush
+
+        if sum_mush == 100:
+            return sum_mush
+        elif abs(100-sum_mush) <= abs(100-closest):
+            closest = sum_mush
+    return closest      
+
+print(mush_sum(mush))
