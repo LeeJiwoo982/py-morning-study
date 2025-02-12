8326b3633e33f62faffb78f7fcbd44da75a99757
ijioo <easywoo980902@gmail.com>
2025-02-13 08:33:30 +0900

    solved bj3052

--- LeeJiwooIM/bj/3052_remain/3052.py
+++ LeeJiwooIM/bj/3052_remain/3052.py
@@ -1,1 +1,11 @@
-git
+N = 10
+arr = [int(input()) for _ in range(N)]  #10개 의 수 입력받기
+# print(arr)
+cnt = [0]*42
+remain = 0
+for i in arr:
+    cnt[i%42]+=1
+for j in range(42):
+    if cnt[j]!=0:
+        remain +=1
+print(remain)
