a90034b4e8993edfe2a5915ff89a03b2d807e4b8
ijioo <easywoo980902@gmail.com>
2025-02-14 08:06:16 +0900

    looling for 8320problem

--- LeeJiwooIM/bj/8320_makeRectangle/8320.py
+++ LeeJiwooIM/bj/8320_makeRectangle/8320.py
@@ -2,9 +2,13 @@
 n =  int(input())
 # 주어진 수로 만들어질 수 있는 정사각형의 개수가 중요.
 # 정사각형의 크기는 k*k
-k = 0
-while k*k <= n:
+k = 1
+
+cnt = n
+while n>=k**2:
     k += 1
-    for i in range(1, k+1):
-        if (i-1)*(i-1) < n <= i * i:
-            pass
+    for i in range(2, k+1):
+        if (i-1)**2 < n <= i ** 2:
+            cnt += n//i -(i-1)
+
+print(cnt)
